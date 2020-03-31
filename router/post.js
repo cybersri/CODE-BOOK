37e3c92090c1616a6338df5cf2c186be68576016
cybersrikanth <ksksrikanthksk@gmail.com>
2020-03-31 23:37:00 +0530

    comments and suggestions

--- router/post.js
+++ router/post.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const { getPost, postPost, patchPost } = require('../controllers/post');
-const { postComment } = require('../controllers/comment')
+
 
 router.get('/post/:id', getPost);
 router.post('/post', postPost);
 router.patch('/post', patchPost);
 
-router.post('/comment', postComment);
 
 module.exports = router;
