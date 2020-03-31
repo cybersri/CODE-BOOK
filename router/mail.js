f0486b521a7c31cf194a179fc26914f8aa0e58da
antonyrajrichard <antony.raj@codingmart.com>
2020-03-31 14:46:27 +0530

    a

--- router/mail.js
+++ router/mail.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-const { getLogin, postLogin } = require('../controllers/login');
 
-router.get('/org/verifymail/:token', (req,res)=>{
+router.get('/org/verifymail/:token', (req, res) => {
     console.log(req.params)
 })
 
