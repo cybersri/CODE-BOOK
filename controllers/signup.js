89ff98cd140de71a1977063787ac3daf8a91bbd9
antonyrajrichard <antony.raj@codingmart.com>
2020-04-01 21:16:14 +0530

    user

--- controllers/signup.js
+++ controllers/signup.js
@@ -12,7 +12,7 @@ exports.getSignup = (req, res, next) => {
 
 exports.postSignup = async (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
         return res.status(422).json({
             errors
         });
@@ -20,30 +20,37 @@ exports.postSignup = async (req, res, next) => {
     const { name, email, phone, address, password, organization } = req.body;
     try {
         console.log(organization)
-        if(!organization){
+        if (!organization) {
             return res.status(403).json({
-                msg:'invalid organization'
+                msg: 'invalid organization'
             })
         }
-        const org = await organizationModel.findOne({name:organization});
-        // if(!org){
-        //     return res.status(403).json({
-        //         msg:'invalid organization'
-        //     })
-        // }
+        const org = await organizationModel.findOne({ name: organization });
         const salt = await bcrypt.genSalt(config.get('SALT'));
         const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new userModel({
-            name, email, phone, address, password: hashedPassword, organization:org
+            name, email, phone, address, password: hashedPassword, organization: org
         });
         await newUser.save();
         return res.status(201).json({
             msg: 'User created'
         })
     } catch (err) {
+        const { name, email, phone, address } = req.body;
+        console.log(req.body)
+        const updatedUser = await userModel.findOneAndUpdate({ email: email, status: 0 },
+            { name, phone, address }
+        );
+        if (updatedUser) {
+            return res.status(201).json({
+                msg: 'User created',
+                updatedUser
+            })
+        }
         return res.status(500).json({
             msg: 'Internal server problem',
-            err: err.message
+            err: err.message,
+            existUser
         })
     }
 }
